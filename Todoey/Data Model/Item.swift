5d7ef5ba40aa7e793222c94707ae18f676be0aeb
Henry Chan <1974hc@gmail.com>
2019-08-27 17:28:12 +0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Henry on 27/8/2019.
+//  Copyright © 2019 illuma works. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
